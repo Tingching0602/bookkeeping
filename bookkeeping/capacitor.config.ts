93ae0b3dc1b872256a620ba495e93a1578beef8d
ANYA\anita <anya.chen@ehs.tw>
2026-04-13 02:17:14 +0800

    包裝成IOS APP

---
+++ bookkeeping/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.pear.bookkeeping',
+  appName: '梨子小帳本',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
